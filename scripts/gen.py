81fb825a650429ad21c4ca012752ed0b6ae40c25
Elliot <git@lazercube.com>
2023-03-09 15:26:40 +0000

    Tweaked package generator

--- scripts/gen.py
+++ scripts/gen.py
@@ -1,45 +1,49 @@
-import pip
-pip.main(['install', 'PyYAML', '--user'])
-
 import bpy
-import os
-import re
-import yaml
-from addon_utils import modules
 
-TAG = "preinstalled"
-PACKAGEFILE = "rocketpack.yaml"
+if bpy.app.background:
+    import pip
+    pip.main(['install', 'PyYAML'])
 
-def make_friendly(s):
-    s = s.replace(' ', '-')
-    s = re.sub(r'[^\w-]', '', s)
-    return s.lower()
+    import os
+    import re
+    import yaml
+    from addon_utils import modules
 
-BLEND_DIR = os.path.dirname(bpy.data.filepath)
-ADDONS_FOLDER = 'addons'
+    TAG = "preinstalled"
+    PACKAGEFILE = "rocketpack.yaml"
 
-# Loop over each addon and write its name to a separate YAML file
-for module in modules():
-    addon_name = module.__name__
-    addon_display_name = module.bl_info.get('name', addon_name)
-    addon_dict = {"addon": {"name": module.__name__}}
+    def make_friendly(s):
+        s = s.replace(' ', '-')
+        s = re.sub(r'[^\w-]', '', s)
+        return s.lower()
 
-    # Determine the type of addon for the folder name
-    addon_folder_category = None
-    addon_support = module.bl_info.get('support', "COMMUNITY")
+    BLEND_DIR = os.path.dirname(bpy.data.filepath)
+    ADDONS_FOLDER = 'addons'
 
-    if addon_support == 'COMMUNITY':
-        addon_folder_category = 'community'
-    elif addon_support == 'OFFICIAL':
-        addon_folder_category = 'blender'
-    else:
-        addon_folder_category = 'unknown'
+    # Loop over each addon and write its name to a separate YAML file
+    for module in modules():
+        addon_name = module.__name__
+        addon_display_name = module.bl_info.get('name', addon_name)
+        addon_dict = {"addon": {"name": module.__name__}}
 
-    # Write the addon dictionary to a YAML file in the appropriate folder
-    addon_folder_name = os.path.join(BLEND_DIR, addon_folder_category, ADDONS_FOLDER, make_friendly(addon_display_name), TAG)
-    if not os.path.exists(addon_folder_name):
-        os.makedirs(addon_folder_name)
+        # Determine the type of addon for the folder name
+        addon_folder_category = None
+        addon_support = module.bl_info.get('support', "COMMUNITY")
 
-    file_name = os.path.join(addon_folder_name, PACKAGEFILE)
-    with open(file_name, 'w') as f:
-        yaml.dump(addon_dict, f, default_flow_style=False)
+        if addon_support == 'COMMUNITY':
+            addon_folder_category = 'community'
+        elif addon_support == 'OFFICIAL':
+            addon_folder_category = 'blender'
+        else:
+            addon_folder_category = 'unknown'
+
+        # Write the addon dictionary to a YAML file in the appropriate folder
+        addon_folder_name = os.path.join(BLEND_DIR, addon_folder_category, ADDONS_FOLDER, make_friendly(addon_display_name), TAG)
+        if not os.path.exists(addon_folder_name):
+            os.makedirs(addon_folder_name)
+
+        file_name = os.path.join(addon_folder_name, PACKAGEFILE)
+        with open(file_name, 'w') as f:
+            yaml.dump(addon_dict, f, default_flow_style=False)
+
+    bpy.ops.wm.quit_blender()
